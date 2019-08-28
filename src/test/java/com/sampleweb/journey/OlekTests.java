d3ca3999ee2e8294a2cc5f60d5a299488dd27997
Rewaz Esmaeel <rewaze@yellowsubmarine.local>
2019-08-28 14:10:01 +0100

    adding tests cases one to five

--- src/test/java/com/sampleweb/journey/OlekTests.java
+++ src/test/java/com/sampleweb/journey/OlekTests.java
@@ -3,6 +3,9 @@ package com.sampleweb.journey;
 import com.sampleweb.BaseTest;
 import com.sampleweb.framework.PageNavigator;
 import com.sampleweb.pages.ChildrensBooksPage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -19,18 +22,84 @@ public class OlekTests extends BaseTest {
         childrensBooksPage = navigator.navigateToPage(driver, ChildrensBooksPage.PATH, ChildrensBooksPage.class);
     }
 
+//    @Test (groups = "regression")
+//    public void babyProductsNavigationPageTest() {
+//        assertThat(childrensBooksPage.isLoaded(), is(true));
+//    }
+//
+//    @Test (groups = "regression")
+//    public void babyProductsUrlNavigationPageTest() {
+//        assertThat(childrensBooksPage.isLoaded(), is(true));
+//    }
+//
+//
+//    @Test (groups = "regression")
+//    public void test() throws Exception {
+//        assertThat(childrensBooksPage.isTitleCorrect(), is(true));
+//    }
+
     @Test (groups = "regression")
-    public void babyProductsNavigationPageTest() {
-        assertThat(childrensBooksPage.isLoaded(), is(true));
+    public void titleNavigationTest() throws Exception {
+        assertThat(childrensBooksPage.isTitleCorrect(), is(true));
     }
 
     @Test (groups = "regression")
-    public void babyProductsUrlNavigationPageTest() {
-        assertThat(childrensBooksPage.isLoaded(), is(true));
+    public void checkTitle() throws Exception {
+        WebElement selectDropDown = driver.findElementByXPath("//select[@aria-describedby=\"searchDropdownDescription\"]");
+
+        Select dropdown = new Select(selectDropDown);
+
+        dropdown.selectByVisibleText("Luxury Beauty");
+
+        WebElement searchButton = driver.findElementByXPath("//*[@id=\"nav-search\"]/form/div[2]/div/input");
+
+        searchButton.click();
+
     }
 
     @Test (groups = "regression")
-    public void searchTest() {
-        childrensBooksPage.setSearchCriteria("Audi lights").clickSubmitButton();
+    public void CheckIfGlobalStoreExist() throws Exception {
+        WebElement globalStoreElem = driver.findElementByXPath("//input[@name=\"s-ref-checkbox-11259240031\"]");
+
+        if(globalStoreElem == null)
+        {
+            Assert.fail();
+        }
     }
+
+    @Test (groups = "regression")
+    public void customerReview() throws Exception {
+        WebElement customerReview = driver.findElementByXPath("//*[@id=\"leftNav\"]/ul[11]");
+
+        if(customerReview == null)
+        {
+            Assert.fail();
+        }
+    }
+
+
+    @Test (groups = "regression")
+    public void firstLinkSecondMenuSameAsCat() throws Exception {
+        WebElement categoryLink = driver.findElementByXPath("//*[@id=\"nav-subnav\"]/a[1]");
+
+        if(categoryLink == null || !categoryLink.getText().equals(childrensBooksPage.getTitle()))
+        {
+            Assert.fail();
+        }
+    }
+
+    @Test (groups = "regression")
+    public void checkLinkSameAsPage() throws Exception {
+        WebElement categoryLink = driver.findElementByXPath("//*[@id=\"nav-subnav\"]/a[1]");
+        categoryLink.click();
+
+        WebElement pageTitle = driver.findElementByXPath("//img[@alt=\"Luxury Beauty\"]");
+
+        if(!pageTitle.getAttribute("alt").equals(childrensBooksPage.getTitle()))
+        {
+            Assert.fail();
+        }
+    }
+
+
 }
