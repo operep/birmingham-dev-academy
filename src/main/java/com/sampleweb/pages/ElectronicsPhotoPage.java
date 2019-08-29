b8d8351560deb8529fc88cf0d273c0bd377ebff5
antoniabadarau <antonia.badarau@gmail.com>
2019-08-29 10:32:15 +0100

    refactor and implement @FindBy(xpath)

--- src/main/java/com/sampleweb/pages/ElectronicsPhotoPage.java
+++ src/main/java/com/sampleweb/pages/ElectronicsPhotoPage.java
@@ -3,22 +3,34 @@ package com.sampleweb.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.FindBy;
 
 import java.util.List;
 
 public class ElectronicsPhotoPage extends HomePage {
 
-    protected String title = "Electronics & Photo";
-    protected String pageTitle = "//h1[text()='The Electronics Store']";
-
+    private String title = "Electronics & Photo";
     public static String PATH = "https://www.amazon.co.uk/s/ref=nb_sb_noss?url=search-alias%3Delectronics&field-keywords=";
     public static String HOME_PATH = "https://www.amazon.co.uk/ref=nav_logo";
 
-    protected String electronicsCategoryDropdown = "//*[@id=\"searchDropdownBox\"]/option[text()='Electronics & Photo']";
-    protected String globalStoreCategory = "//h4[text()='Global Store']";
-    protected String avgCustomerReviewCategory = "//h4[text()='Avg. Customer Review']";
-    protected String secondLineLink = "//*[@class='nav-a-content' and contains(text(),'Electronics')]";
-    protected String primeCheckbox = "//*[@class='a-icon a-icon-prime a-icon-small s-ref-text-link']";
+    @FindBy(xpath = "//h1[text()='The Electronics Store']")
+    protected WebElement pageTitle;
+
+    @FindBy(xpath = "//*[@id=\"searchDropdownBox\"]/option[text()='Electronics & Photo']")
+    protected WebElement electronicsCategoryDropdown;
+
+    @FindBy(xpath = "//h4[text()='Global Store']")
+    protected WebElement globalStoreCategory;
+
+    @FindBy(xpath = "//h4[text()='Avg. Customer Review']")
+    protected WebElement avgCustomerReviewCategory;
+
+    @FindBy(xpath = "//*[@class='nav-a-content' and contains(text(),'Electronics')]")
+    protected WebElement secondLineLink;
+
+    @FindBy(xpath = "//*[@class='a-icon a-icon-prime a-icon-small s-ref-text-link']")
+    protected WebElement primeCheckbox;
+
     protected String listOfElements = "//*[@class='s-result-list s-search-results sg-row']/div";
     protected String labelsInDescription = "//*[@aria-label='Amazon Prime' or text()='FREE Delivery by Amazon']";
 
@@ -29,27 +41,27 @@ public class ElectronicsPhotoPage extends HomePage {
 
     // 1
     public boolean isElectronicsCategorySelected(){
-        return driver.findElement(By.xpath(electronicsCategoryDropdown)).isSelected();
+        return electronicsCategoryDropdown.isSelected();
     }
 
     // 1
     public boolean isPageLoaded(){
-        return driver.findElement(By.xpath(pageTitle)).isDisplayed();
+        return pageTitle.isDisplayed();
     }
 
     // 2
     public boolean isGlobalStoreDisplayed(){
-        return driver.findElement(By.xpath(globalStoreCategory)).isDisplayed();
+        return globalStoreCategory.isDisplayed();
     }
 
     // 3
     public boolean isAvgCustomerReviewDisplayed(){
-        return driver.findElement(By.xpath(avgCustomerReviewCategory)).isDisplayed();
+        return avgCustomerReviewCategory.isDisplayed();
     }
 
     // 4
     public boolean isSecondLineLinkDisplayed(){
-        return driver.findElement(By.xpath(secondLineLink)).isDisplayed();
+        return secondLineLink.isDisplayed();
     }
 
     // 5
@@ -74,7 +86,7 @@ public class ElectronicsPhotoPage extends HomePage {
     }
 
     private void checkPrimeCheckbox(){
-        driver.findElement(By.xpath(primeCheckbox)).click();
+        primeCheckbox.click();
     }
 
     // gets the title of the initial page
@@ -84,19 +96,19 @@ public class ElectronicsPhotoPage extends HomePage {
 
     // gets the tile of the reloaded page
     private String getReloadSecondLineLinkTitle(){
-        driver.findElementByXPath(secondLineLink).click();
+        secondLineLink.click();
         return driver.getTitle();
     }
 
     // selects the Electronics & Photo category from the dropdown
     public void selectElectronicsCategory(){
-        driver.findElementByXPath(electronicsCategoryDropdown).click();
-        emptySearch();
+        electronicsCategoryDropdown.click();
+        search("");
     }
 
     // executes empty search
-    private void emptySearch(){
-        getSearchField().sendKeys("");
+    private void search(String searchCriteria){
+        getSearchField().sendKeys(searchCriteria);
         clickSubmitButton();
     }
 }
