9e66431af0536ead83983111fda461ea49ab51ca
Shuaib Siddique <shuaibsiddique@Viara.local>
2019-08-29 10:41:12 +0100

    removed unused imports

--- src/main/java/com/sampleweb/pages/GardenAndOutdoors.java
+++ src/main/java/com/sampleweb/pages/GardenAndOutdoors.java
@@ -2,10 +2,7 @@ package com.sampleweb.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.Select;
 
 import java.util.ArrayList;
 
