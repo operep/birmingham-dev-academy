5f6e0bc311858c378acee4dd988898a01ca7d398
Jennifer Myers <jenniferm@waferthin.local>
2019-08-29 10:09:48 +0100

    added begining of test for getting average items in order and some refractoring

--- src/main/java/com/sampleweb/pages/HealthPage.java
+++ src/main/java/com/sampleweb/pages/HealthPage.java
@@ -8,12 +8,17 @@ import javax.xml.xpath.XPath;
 
 public class HealthPage extends HomePage {
 
-    public String title = "Health & Personal Care";
-    public String pageTitle = "//h1[text()=\"Health & Personal Care\"]";
-    public static String PATH = "https://www.amazon.co.uk/s/ref=nb_sb_noss?url=search-alias%3Ddrugstore&field-keywords=";
-    public String pageTitleHead = "//*[@id=\"nav-subnav\"]/a[1]/span";
-    public String globalStore = "//*[@id=\"leftNav\"]/h4[2]";
-    public PageNavigator navigator = new PageNavigator();
+    private String title = "Health & Personal Care";
+    private PageNavigator navigator = new PageNavigator();
+
+    private String pageTitle = "//h1[text()=\"Health & Personal Care\"]";
+    private static String PATH = "https://www.amazon.co.uk/s/ref=nb_sb_noss?url=search-alias%3Ddrugstore&field-keywords=";
+    private String pageTitleHead = "//*[@id=\"nav-subnav\"]/a[1]/span";
+    private String globalStore = "//*[@id=\"leftNav\"]/h4[2]";
+    private String dropDownMenu = "//*[@id=\"searchDropdownBox\"]";
+    private String searchButton = "//*[@id=\"nav-search\"]/form/div[2]/div";
+
+
 
     public HealthPage(RemoteWebDriver driver) {
         super(driver);
@@ -21,9 +26,9 @@ public class HealthPage extends HomePage {
 
     public void navigateToHealthPage() throws Exception {
         navigator.navigateToPage(driver, "https://www.amazon.co.uk", HealthPage.class);
-        driver.findElementByXPath("//*[@id=\"searchDropdownBox\"]").click();
-        driver.findElementByXPath("//*[@id=\"searchDropdownBox\"]/option[25]").click();
-        driver.findElementByXPath("//*[@id=\"nav-search\"]/form/div[2]/div").click();
+        driver.findElementByXPath(dropDownMenu).click();
+        driver.findElementByXPath(dropDownMenu + "/option[25]").click();
+        driver.findElementByXPath().click();
     }
 
     public boolean isHealthPageLoaded() throws Exception {
@@ -66,9 +71,19 @@ public class HealthPage extends HomePage {
     public boolean verifySelectPrimeReturnsOnlyPrimeitems() throws Exception {
         navigateToHealthPage();
 
-        driver.findElementByXPath("//*[@id=\"leftNav\"]/ul[6]/div/li[1]/span/span/div/label/input").click();
+        driver.findElementByXPath("//*[@id='leftNav']/ul[6]/div/li[1]/span/span/div/label/input").click();
 
             return driver.findElements(By.xpath("//*[@class='a-section aok-relative s-image-fixed-height']")).iterator().next().
                     findElement(By.xpath("//*[@class='a-icon a-icon-prime a-icon-medium']")).isDisplayed();
-        }
+    }
+
+    public boolean verifyAverageRatingsSortedCorrectly() throws Exception {
+        navigateToHealthPage();
+        driver.findElementByXPath("//*[@id='leftNav']/ul[9]/div/li[1]/span/a/span").click();
+        driver.findElementByXPath("//*[@id='a-autoid-0-announce']/span[1]").click();
+        driver.findElementByXPath("//*[@id='s-result-sort-select_3']").click();
+        driver.findElementByXPath("//*[@id='search']/div[1]/div[2]/div/span[3]/div[1]/div[1]/div/div/div/div[2]/div[2]/div/div[1]/div/div/div[2]/div/span/a/i[1]/span").click();
+        driver.findElementsByXPath("//*[@class='a-icon-alt']").iterator().next().getText().
+    }
+
     }
